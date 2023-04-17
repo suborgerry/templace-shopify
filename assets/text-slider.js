703d764b2ad4a6357541176b7835ff7140d0f71e
borgerry <gerryborodin@gmail.com>
2023-04-17 14:13:20 +0300

    Text slider

---
+++ assets/text-slider.js
@@ -0,0 +1,27 @@
+(() => {
+    const textSlider = document.querySelector('.text-slider');
+    const parentHeight = textSlider.clientHeight;
+    const sliderItems = textSlider.querySelectorAll('.text-slider__slide');
+
+    sliderItems.forEach(item => {
+        item.style = `--parent-height: ${parentHeight}`
+    });
+
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 3,
+        direction: 'vertical',
+        freeMode: true,
+        enabled: false,
+        breakpoints: {
+            990: {
+                enabled: true,
+                slidesPerView: 3.25,
+                centeredSlides: true,
+                initialSlide: 2,
+                direction: 'horizontal'
+            }
+        }
+    });
+
+
+})();
