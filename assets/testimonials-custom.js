4a19372427bd7524008476f86fb07d54acacf4ce
borgerry <gerryborodin@gmail.com>
2023-04-19 17:06:43 +0300

    payment methods

--- assets/testimonials-custom.js
+++ assets/testimonials-custom.js
@@ -1,22 +1,18 @@
 (() => {
-    // Получаем все элементы с классом "vertical-testimonials__slide"
     const slides = document.querySelectorAll('.vertical-testimonials__slide');
 
-    // Инициализируем переменную для хранения максимальной высоты
     let maxHeight = 0;
 
-    // Перебираем все слайды и находим максимальную высоту
     slides.forEach(slide => {
         const slideHeight = slide.offsetHeight;
         if (slideHeight > maxHeight) {
             maxHeight = slideHeight;
         }
     });
-    console.log(maxHeight);
+
     if (window.innerWidth <= 768) {
         maxHeight = maxHeight + 80
     }
-    console.log(maxHeight);
 
     // Находим элемент с классом "vertical-testimonials-swiper" и назначаем ему максимальную высоту
     const verticalTestimonialsSwiper = document.querySelector('.vertical-testimonials-swiper');
